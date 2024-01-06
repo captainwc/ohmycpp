17777f5e9585dea56ff7a5f84f44fd4e3f1d0ac8
shuaikai <shuaikai@kk>
2024-01-07 02:44:02 +0800

    (feature) 简易日志功能

--- include/algorithm/Utils.h
+++ include/algorithm/Utils.h
@@ -12,6 +12,7 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <regex>
@@ -28,6 +29,7 @@ namespace utils {
 #define FC_RED     "\033[31m"
 #define FC_GREEN   "\033[32m"
 #define FC_YELLOW  "\033[33m"
+#define FC_GREY    "\033[90m"
 
 enum COLOR {
     RED,
@@ -96,20 +98,35 @@ enum LEVEL {
     ERROR
 };
 
+#define LOG(level, format, ...) utils::log::logWithPos(level, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define ERROR(format, ...)      LOG(utils::log::ERROR, format, ##__VA_ARGS__)
+#define DEBUG(format, ...)      LOG(utils::log::DEBUG, format, ##__VA_ARGS__)
+#define WARN(format, ...)       LOG(utils::log::WARN, format, ##__VA_ARGS__)
+
 // TODO 新增 log 到文件
-inline void log(LEVEL level, const char* format, ...) {
+// TODO 把c++哪个什么forward传递参数的给整明白，tnnd，一定要写出一个能输出位置的日志函数
+inline void logWithPos(LEVEL level, const char* filePath, const char* funcName, int line, const char* format, ...) {
+    std::filesystem::path path(filePath);
+    std::string fileName = path.filename().string();
     switch (level) {
         case ERROR:
-            printf(FC_RED);
-            printf("[%s][%s] ", "ERROR", time::getFormatTime().c_str());
+            std::cout << FC_RED << "[ERROR]" << FC_GREY << "[" << time::getFormatTime().c_str() << "]"
+                      << "[" << fileName.c_str() << ":" << funcName << ":" << line << "] " << FC_RED;
+            // printf(FC_RED);
+            // printf("[%s][%s][%s:%s:%d] ", "ERROR", time::getFormatTime().c_str(), fileName.c_str(),
+            // funcName, line);
             break;
         case WARN:
-            printf(FC_YELLOW);
-            printf("[%s][%s] ", "WARN ", time::getFormatTime().c_str());
+            std::cout << FC_YELLOW << "[WARN ]" << FC_GREY << "[" << time::getFormatTime().c_str() << "]"
+                      << "[" << fileName.c_str() << ":" << funcName << ":" << line << "] " << FC_YELLOW;
+            // printf(FC_YELLOW);
+            // printf("[%s][%s][%s:%s:%d] ", "WARN ", time::getFormatTime().c_str(), fileName.c_str(), funcName, line);
             break;
         case DEBUG:
         default:
-            printf("[%s][%s] ", "DEBUG", time::getFormatTime().c_str());
+            std::cout << "[DEBUG]" << FC_GREY << "[" << time::getFormatTime().c_str() << "]"
+                      << "[" << fileName.c_str() << ":" << funcName << ":" << line << "] " << ANSI_RESET;
+            // printf("[%s][%s][%s:%s:%d] ", "DEBUG", time::getFormatTime().c_str(), fileName.c_str(), funcName, line);
             break;
     }
     va_list args;
@@ -119,6 +136,11 @@ inline void log(LEVEL level, const char* format, ...) {
     printf(ANSI_RESET);
     putchar('\n');
 }
+
+// template <typename... Args>
+// inline void log(log::LEVEL level, const char* format, Args... args) {
+//     logWithPos(level, "", "", 0, format, args...);
+// }
 }  // namespace log
 
 //==-------------------- Print ----------------------==//
--- test/tools/loggerTest.cpp
+++ test/tools/loggerTest.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    utils::log::log(utils::log::ERROR, "%s %d", "hello", 1);
-    utils::log::log(utils::log::WARN, "%s %d", "hello", 1);
-    utils::log::log(utils::log::DEBUG, "%s %d", "hello", 1);
+    utils::log::logWithPos(utils::log::ERROR, __FILE__, __FUNCTION__, __LINE__, "%s %d", "hello", 1);
+    LOG(utils::log::WARN, "%s %d", "hello", 1);
+    ERROR("%s %d", "hello", 2);
+    WARN("%s %d", "hello", 2);
+    DEBUG("%s %d", "hello", 2);
+    // utils::log::log(utils::log::DEBUG, "%s %d", "hello", 1);
 
     return 0;
 }

9af18f42d6479f960f0c285b974e644aea8fc858
wddjwk <wddjwk@qq.com>
2024-03-10 16:57:00 +0800

    (config)适配windows环境

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,5 +1,7 @@
-# basic config
+# 基本设置
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
+# 基本路径设置
 set(CMAKE_BINARY_DIR ${CMAKE_SOURCE_DIR}/build)
 set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin)
 set(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
@@ -7,7 +9,7 @@ set(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
 set(LOG_DIR ${CMAKE_SOURCE_DIR}/log)
 set(CPP_CHECK_DIR ${LOG_DIR}/cppcheck)
 
-# Project Module
+# 项目模块开关
 option(BUILD_SRC "Add leetcode SRC target" ON)
 option(BUILD_TEST "Build Example and Test" ON)
 
@@ -22,7 +24,7 @@ if(BUILD_TEST)
     add_subdirectory(test)
 endif()
 
-# CXX
+# CXX相关设置
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -Wall")
 
 option(WERROR "if -Werror" ON)
@@ -30,4 +32,5 @@ if(WERROR)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
 endif()
 
+# 依赖
 find_program(CPP_CHECK "cppcheck")
